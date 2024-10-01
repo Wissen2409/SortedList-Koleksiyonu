672cd07f72e38f2a5c39243d92944ef34dccb0a2
emrahelis <emrahelis@gmail.com>
2024-10-01 19:30:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// SortedList koleksiyonu  : Sıralı liste koleksiyonu
+
+// SortedList, bugüne kadar gördüğümüz koleksiyonlardan, farklı olarak Key ve Value çifti taşırlar
+
+// Syntax : 
+
+using System.Collections;
+
+SortedList sortedList = new SortedList();
+sortedList.Add(9,"Ali");
+sortedList.Add(6,"Hande");
+sortedList.Add(1,"Elif");
+sortedList.Add(7,"Selçuk");
+sortedList.Add(7,"Handan");
+
+// SortedList koleksiyonu, key ve value değerini, object tipinden alır.
+// object tipinde aldığı için, değeri int yada string girebiliriz.
+
+// Sortedlist, adından da anlaşılacağı üzere, girilen key değerlerine göre, sıralama işlemi yapar.
+// verileri karışık girseniz bile, hiç müdahale etmeden, key değerine göre sıralar.
+// key değeri eğer sayısal bir değerse, küçükten büyüğe doğru, harf ise, alfabetik sıra ile sıralayacaktır.
+// SortedList ekrana yazdırma
+/*for(int i=0;i<sortedList.Count;i++){
+
+    // GetByIndex fonksiyonu ile , sortedlist koleksiyonunu ekranda gösterdik
+    string value = sortedList.GetByIndex(i).ToString();
+    Console.WriteLine(value);
+}*/
+
+// Key ve Value taşıdığı için Contais(Arama) fonksiyonu iki tanedir.
+// ContainsKey ve ContainsValue fonksiyonları key arama ve value arama için kullanılır.
+
+// Örnek : 
+
+bool isContains = sortedList.ContainsKey(90);
+Console.WriteLine(isContains);
